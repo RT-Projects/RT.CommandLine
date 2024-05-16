2367586e6be2d90c1ffba8db2a3294e6675b93b3
Timwi <github@timwi.de>
2024-05-16 21:14:02 +0200

    Add a couple more tests, in particular, one for the -- feature that makes subsequent parameters not be parsed as options

--- Tests/CommandLineTests.cs
+++ Tests/CommandLineTests.cs
@@ -5,7 +5,14 @@ namespace RT.CommandLine.Tests;
 public sealed class CmdLineTests
 {
 #pragma warning disable 0649 // Field is never assigned to, and will always have its default value null
-    private class commandLine
+    private class CommandLine1
+    {
+        [IsPositional, IsMandatory]
+        public string Arg;
+        [Option("-o")]
+        public string Option;
+    }
+    private class CommandLine2
     {
         [Option("--stuff")]
         public string Stuff;
@@ -17,21 +24,57 @@ public sealed class CmdLineTests
     [Fact]
     public static void Test()
     {
-        var c = CommandLineParser.Parse<commandLine>("--stuff blah abc def".Split(' '));
-        Assert.Equal("blah", c.Stuff);
-        Assert.True(c.Args.SequenceEqual(new[] { "abc", "def" }));
+        try
+        {
+            CommandLineParser.Parse<CommandLine1>([]);
+            Assert.Fail();
+        }
+        catch (CommandLineParseException e)
+        {
+            Assert.Equal("The parameter <Arg> is mandatory and must be specified.", e.Message);
+        }
+        try
+        {
+            CommandLineParser.Parse<CommandLine1>(["-o", "Option"]);
+            Assert.Fail();
+        }
+        catch (CommandLineParseException e)
+        {
+            Assert.Equal("The parameter <Arg> is mandatory and must be specified.", e.Message);
+        }
 
-        c = CommandLineParser.Parse<commandLine>("def --stuff thingy abc".Split(' '));
-        Assert.Equal("thingy", c.Stuff);
-        Assert.True(c.Args.SequenceEqual(new[] { "def", "abc" }));
+        var c1 = CommandLineParser.Parse<CommandLine1>(["Arg"]);
+        Assert.Equal("Arg", c1.Arg);
+        Assert.Null(c1.Option);
 
-        c = CommandLineParser.Parse<commandLine>("--stuff stuff -- abc --stuff blah -- def".Split(' '));
-        Assert.Equal("stuff", c.Stuff);
-        Assert.True(c.Args.SequenceEqual(new[] { "abc", "--stuff", "blah", "--", "def" }));
+        c1 = CommandLineParser.Parse<CommandLine1>(["-o", "Arg1", "Arg2"]);
+        Assert.Equal("Arg1", c1.Option);
+        Assert.Equal("Arg2", c1.Arg);
+
+        c1 = CommandLineParser.Parse<CommandLine1>(["Arg1", "-o", "Arg2"]);
+        Assert.Equal("Arg1", c1.Arg);
+        Assert.Equal("Arg2", c1.Option);
+
+        c1 = CommandLineParser.Parse<CommandLine1>(["--", "-o"]);
+        Assert.Equal("-o", c1.Arg);
+        Assert.Null(c1.Option);
+
+
+        var c2 = CommandLineParser.Parse<CommandLine2>("--stuff blah abc def".Split(' '));
+        Assert.Equal("blah", c2.Stuff);
+        Assert.True(c2.Args.SequenceEqual(["abc", "def"]));
+
+        c2 = CommandLineParser.Parse<CommandLine2>("def --stuff thingy abc".Split(' '));
+        Assert.Equal("thingy", c2.Stuff);
+        Assert.True(c2.Args.SequenceEqual(["def", "abc"]));
+
+        c2 = CommandLineParser.Parse<CommandLine2>("--stuff stuff -- abc --stuff blah -- def".Split(' '));
+        Assert.Equal("stuff", c2.Stuff);
+        Assert.True(c2.Args.SequenceEqual(["abc", "--stuff", "blah", "--", "def"]));
 
         try
         {
-            CommandLineParser.Parse<commandLine>("--blah stuff".Split(' '));
+            CommandLineParser.Parse<CommandLine2>("--blah stuff".Split(' '));
             Assert.Fail();
         }
         catch (CommandLineParseException e)
